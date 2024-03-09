fedaefe6a178e6529736d9a52cda0d496e296bee
protim1451 <partha.doict@gmail.com>
2024-03-09 15:33:51 +0600

    friend added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import Counter from './counter'
 import Team from './Team'
 import Users from './Users'
+import Friends from './Friends'
+import Friend from './Friend'
 
 function App() {
   
@@ -17,7 +19,8 @@ function App() {
   return (
     <>
       <h1>React Core concept 2</h1>
-
+      
+      <Friends></Friends>
       <Users></Users>
       <Team></Team>
       <Counter></Counter>
